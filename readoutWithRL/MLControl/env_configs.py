f085c80ef51d8933435da5c6d55c4bbc47fe7caa
AnikenC <quantac47@gmail.com>
2024-07-29 11:35:33 +0530

    fixed leak

--- readoutWithRL/MLControl/env_configs.py
+++ readoutWithRL/MLControl/env_configs.py
@@ -1,8 +1,37 @@
 import copy
+from typing import Optional
 import jax.numpy as jnp
 
 # Sherbrooke Q1 Sim Config
-_sherbrooke_sim_config = {
+_sherbrooke_q0_sim_config = {
+    "kappa": 14.31,
+    "chi": 0.335 * 2.0 * jnp.pi,
+    "kerr": 0.00,
+    "time_coeff": 1.0,
+    "snr_coeff": 20.0,
+    "smoothness_coeff": 1.0,
+    "smoothness_baseline_scale": 1.0,
+    "gauss_kernel_len": 15,
+    "gauss_kernel_std": 2.0,
+    "bandwidth": 50.0,
+    "freq_relative_cutoff": 0.1,
+    "bandwidth_coeff": 0.0,
+    "n0": 53.8,
+    "tau_0": 0.783,
+    "res_amp_scaling": 1 / 0.348,
+    "nR": 0.05,
+    "snr_scale_factor": 1.25,
+    "gamma_I": 1 / 362.9,
+    "photon_gamma": 1 / 4000,
+    "sim_t1": 0.63,
+    "init_fid": 1.0,
+    "photon_weight": 12.0,
+    "standard_fid": 0.99,
+    "shot_noise_std": 0.0,
+}
+
+# Sherbrooke Q1 Sim Config
+_sherbrooke_q1_sim_config = {
     "kappa": 14.31,
     "chi": 0.31 * 2.0 * jnp.pi,
     "kerr": 0.00,
@@ -58,8 +87,11 @@ _kyoto_sim_config = {
 }
 
 
-def get_sherbrooke_config():
-    return copy.deepcopy(_sherbrooke_sim_config)
+def get_sherbrooke_config(qubit: Optional[int] = 0):
+    if qubit == 0:
+        return copy.deepcopy(_sherbrooke_q0_sim_config)
+    if qubit == 1:
+        return copy.deepcopy(_sherbrooke_q1_sim_config)
 
 
 def get_kyoto_config():
